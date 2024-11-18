119d884d8cd2eedb33409084db9319ac7683e360
Vishal Gautam <ivishalgautamm@gmail.com>
2024-11-18 23:12:31 +0530

    init

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,6 +1,5 @@
 "use client";
 import About from "@/components/About";
-import Blogs from "@/components/Blogs";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Industries from "@/components/Industries";
