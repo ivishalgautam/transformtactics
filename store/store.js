02f791f7a99c20f8de4d9ee79c69103fc6d7b277
Vishal Gautam <ivishalgautamm@gmail.com>
2024-11-18 23:04:41 +0530

    init

--- store/store.js
+++ store/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productsSlice from "./features/productsSlice";
 import modalSlice from "./features/modalSlice";
 
 export const store = configureStore({
